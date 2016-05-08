8982cee0768d796bf96caa45429a76906c71f0ba
Filippo Squillace <squillace.filippo@gmail.com>
2016-05-08 19:13:26 +0100

    Use function inside trap definition

--- pearl-metadata/config.fish
+++ pearl-metadata/config.fish
@@ -1,9 +1,11 @@
 
-# Cleanup temp directory
+# trap for OSX works differently due to the BSD getopt.
+# With function things works fine with both GNU and BSD version:
+# https://github.com/fish-shell/fish-shell/issues/607
 function pearl_remove_tmp --on-process-exit %self
     /bin/rm -fr $PEARL_TEMPORARY
 end
 # The following looks not working:
-trap "/bin/rm -fr $PEARL_TEMPORARY" QUIT ABRT KILL TERM
+trap pearl_remove_tmp QUIT ABRT KILL TERM
 
 # vim: ft=sh
